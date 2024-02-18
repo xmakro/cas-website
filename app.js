1b34500ba08e42bbaa58fccce2b66561cc4cdb90
xmakro <makro@>
2024-02-18 12:05:56 +0100

    Initial

---
+++ app.js
@@ -0,0 +1,43 @@
+function toggleNav() {
+    document.body.classList.toggle('open');
+}
+
+function closeNav() {
+    document.body.classList.remove('open');
+}
+
+function load() {
+    var cur = location.hash.substring(1);
+    if (!cur) cur = 'start';
+
+    // Stamp content.
+    let template = document.querySelector('#' + cur);
+    let clone = document.importNode(template.content, true);
+    let content = document.querySelector('#content');
+    content.innerHTML = '';
+    content.appendChild(clone);
+
+    // Update links.
+    for (let e of document.querySelectorAll('a')) {
+        if (e.getAttribute('href') == '#' + cur) {
+            e.classList.add('active');
+        } else {
+            e.classList.remove('active');
+        }
+        e.addEventListener('click', closeNav);
+    }
+
+    // Navigation toggle.
+    let navtoggle = document.querySelector('#navtoggle');
+    navtoggle.addEventListener('click', toggleNav);
+}
+
+window.onhashchange = load;
+window.onload = load;
+
+// Scroll overlay.
+const storeScroll = () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+};
+document.addEventListener('scroll', storeScroll);
+storeScroll();
